9f62fd277900ab04f583a559df41c97320b24f10
MikeWeiZhou <mike.weizhou@gmail.com>
2018-02-09 18:32:40 -0800

    Added getByName() to Categories model

--- application/models/Categories.php
+++ application/models/Categories.php
@@ -8,6 +8,22 @@ class Categories extends CSV_Model
     // Constructor
     public function __construct()
     {
-        parent::__construct('../data/categories.csv', 'categoryid');
+        parent::__construct('../data/category.csv', 'categoryid');
+    }
+
+    /**
+     * Get record by name, or return null.
+     */
+    public function getByName($name)
+    {
+        $everything = $this->all();
+        for ($i = 0; $i < sizeof($everything); ++$i)
+        {
+            if ($everything[$i]->dirname == $name)
+            {
+                return $everything[$i];
+            }
+        }
+        return null;
     }
 }
