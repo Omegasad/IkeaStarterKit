cd98283f91a68c60a3edb41541725f7214b03a3e
MikeWeiZhou <mike.weizhou@gmail.com>
2018-02-09 23:56:28 -0800

    Added Sets model and Testsets controller

---
+++ application/models/Sets.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Sets of accessories.
+ */
+class Sets extends CSV_Model
+{
+    // Constructor
+    public function __construct()
+    {
+        parent::__construct('../data/set.csv', 'setid');
+    }
+
+    /**
+     * Get set by name.
+     */
+    public function getByName($name)
+    {
+        $everything = parent::all();
+        for ($i = 0; $i < sizeof($everything); ++$i)
+        {
+            if ($everything[$i]->setname == $name)
+            {
+                return $everything[$i];
+            }
+        }
+        return null;
+    }
+}
