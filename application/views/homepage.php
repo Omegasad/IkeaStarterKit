104003b5ff1484ef098d1e7fa999ca0727017703
Omegasad <tkahoso@my.bcit.ca>
2018-02-10 10:35:53 -0800

    fixed catalog linking and moved index information to catalog

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -9,15 +9,3 @@
 </div>
 </h1>
 
-<div id="row">
-    {datasets}
-    <div class="span1">
-    <a href="/selectset/{setid}">SetID: {setid}</a><br />
-    SetName: {setname}<br />
-    SofaID: {sofaid}<br />
-    TableID: {tableid}<br />
-    LampID: {lampid}<br />
-    PaintingID: {paintingid}<br /><br /><br />
-    </div>
-    {/datasets}
-</div>
