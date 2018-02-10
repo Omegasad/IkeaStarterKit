f4d4677988a0debb7a72c9e06337c57e3a2920fd
MikeWeiZhou <mike.weizhou@gmail.com>
2018-02-10 15:11:49 -0800

    Removed unused files and moved menu to template.php

--- application/views/template.php
+++ application/views/template.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>{pagetitle}</title>
+    <head>
+        <meta charset="utf-8">
+        <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
-	</head>
-	<body>
-        <div id="container">
-			{content}
-        </div>
-	</body>
+    </head>
+    <body>
+    <div id="nav">
+        <a href="/">Home</a>
+        <a href="/catalog">Catalog</a>
+    </div>
+    <div id="container">{content}</div>
+    </body>
 </html>
